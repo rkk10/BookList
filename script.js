763cecb2a9308c5abdc328d2d35695e39d03c5c4
Ravindra Khandagale <ravindrakk143@gmail.com>
2018-01-26 23:24:50 +0530

    script file erased totally

--- script.js
+++
@@ -1,8 +0,0 @@
-
-var books = document.querySelectorAll('#list li h4')
-
-Array.from(books).forEach(function(book){
-    book.textContent += ' (book title)';
-})
-
-const bookList = document.getElementById("bookList")
