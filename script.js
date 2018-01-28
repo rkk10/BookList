7630edd2d7205fce1011a440c073d8f0d56aaa66
Ravindra Khandagale <ravindrakk143@gmail.com>
2018-01-28 18:50:22 +0530

    code for dom remove is done and for dom add is going on

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ list.addEventListener('click', (e)=>{
         list.removeChild(li)
        }
 })
+
+// add books
+
+cont add = document.forms['add_book']
+add.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const newBook = add.querySelector('input[type = "text"]').value
+    const newLi = document.createElement('li')
+    const h4 = document.createElement('h4')
+    const newDel = document.createComment('button')
+    newDel.classList('del')
+})
