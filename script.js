10b2648f99c574878ec8267a9714a5946cdd4362
Ravindra Khandagale <ravindrakk143@gmail.com>
2018-01-30 00:41:10 +0530

    code done for books search

--- script.js
+++ script.js
@@ -44,3 +44,19 @@ hide.addEventListener('change', (e)=>{
         list.style.display = 'initial'
     }
 })
+
+//searh Books
+const search = document.getElementById('search')
+search.addEventListener('keyup',(e)=>{
+    const term = e.target.value.toLowerCase()
+    const books = list.getElementsByTagName('li')
+    Array.from(books).forEach((book)=>{
+        const title = book.firstElementChild.textContent
+        if(title.toLocaleLowerCase().indexOf(term)!=-1){
+            book.style.display = 'block'
+        }
+        else{
+            book.style.display = 'none'
+        }
+    })
+})
