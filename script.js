8566a7b2c4c3adbed6fe8883d3bd85d4f969fdde
Ravindra Khandagale <ravindrakk143@gmail.com>
2018-01-29 13:02:12 +0530

    insert new book code is done

--- script.js
+++ script.js
@@ -11,12 +11,19 @@ list.addEventListener('click', (e)=>{
 
 // add books
 
-cont add = document.forms['add_book']
+const add = document.forms['add_book']
 add.addEventListener('submit',(e)=>{
     e.preventDefault();
+    const list = document.getElementById('list')
     const newBook = add.querySelector('input[type = "text"]').value
     const newLi = document.createElement('li')
     const h4 = document.createElement('h4')
-    const newDel = document.createComment('button')
-    newDel.classList('del')
+    h4.textContent = newBook
+    const newDel = document.createElement('button')
+    newDel.textContent = 'Delete'
+    newDel.classList.add('del')
+    newLi.appendChild(h4)
+    newLi.appendChild(newDel)
+    list.appendChild(newLi)
+    
 })
